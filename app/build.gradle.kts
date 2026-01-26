1e356656bda2b116426d1bc5468f53ac41ebe29f
sLingli <YS111165542@outlook.com>
2026-01-26 22:51:16 +0800

    v2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "2.3"
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
