420b50376911f5e6b0aafbe1e23fb32a1a3c93ed
sLingli <YS111165542@outlook.com>
2026-02-18 21:53:41 +0800

    Update Gradle and plugin versions for improved compatibility and performance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10"
 }
 
 android {
